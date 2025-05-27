bb96d9b958e2f4d465a2e6795c0b935ec246c286
AnniNri <143839947+AnniNri@users.noreply.github.com>
2025-05-27 22:03:52 +0530

    Time: 1 ms (74.69%), Space: 40.5 MB (96.41%) - LeetHub

---
+++ 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int differenceOfSums(int n, int m) {
+        int sum1 = 0;
+
+        // Loop through all integers from 1 to n
+        for (int i = 1; i <= n; i++) {
+            // If the current number is divisible by m
+            if (i % m == 0) {
+                sum1 -= i; // Subtract it from sum1
+            } else {
+                sum1 += i; // Otherwise, add it to sum1
+            }
+        }
+
+        // Return the final calculated value
+        // which is: (sum of all numbers from 1 to n not divisible by m)
+        // minus (sum of all numbers from 1 to n divisible by m)
+        return sum1;
+    }
+}
